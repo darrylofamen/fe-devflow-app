bc658642220655abc988e14571282b2f6c2ee04e
Darryl <darryl.ofamen@gmail.com>
2025-11-16 19:29:01 +0800

    Implement Tag Input

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Badge } from "@/components/ui/badge";
 import { getDeviconClassName } from "@/lib/utils";
 import ROUTES from "@/constants/routes";
+import Image from "next/image";
 
 interface Props {
   _id: string;
@@ -10,22 +11,48 @@ interface Props {
   questions?: number;
   showCount?: boolean;
   compact?: boolean;
+  remove?: boolean;
+  isButton?: boolean;
+  handleRemove?: () => void;
 }
 
-const TagCard = ({ _id, name, questions, showCount, compact }: Props) => {
+const TagCard = ({ _id, name, questions, showCount, compact, remove, isButton, handleRemove }: Props) => {
   const iconClass = getDeviconClassName(name);
 
-  return (
-    <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
-      <Badge className="background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+  const Content = (
+    <>
+      <Badge className="background-light800_dark300 text-light400_light500 flex flex-row gap-2 rounded-md border-none px-4 py-2 uppercase">
         <div className="flex-center space-x-2">
           <i className={`${iconClass} text-sm`} />
           <span>{name}</span>
         </div>
+
+        {remove && (
+          <Image
+            src="/icons/close.svg"
+            alt="Close Icon"
+            width={12}
+            height={12}
+            className="cursor-pointer object-contain invert-0 dark:invert"
+            onClick={handleRemove}
+          />
+        )}
       </Badge>
 
       {showCount && <p className="small-medium text-dark500_light700">{questions}</p>}
-    </Link>
+    </>
   );
+
+  if (compact) {
+    return isButton ? (
+      <button className="flex justify-between gap-2" type="button">
+        {Content}
+      </button>
+    ) : (
+      <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+        {Content}
+      </Link>
+    );
+  }
 };
 export default TagCard;
