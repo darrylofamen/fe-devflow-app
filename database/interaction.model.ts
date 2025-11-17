11f391107691d16ca35f0bc7831150e95a316ffa
Darryl <darryl.ofamen@gmail.com>
2025-11-17 19:39:27 +0800

    Create Interaction model

---
+++ database/interaction.model.ts
@@ -0,0 +1,22 @@
+import { Document, model, models, Schema, Types } from "mongoose";
+
+export interface IInteraction extends Document {
+  user: Types.ObjectId;
+  action: string;
+  actionId: Types.ObjectId;
+  actionType: "question" | "answer";
+}
+
+const interactionSchema = new Schema<IInteraction>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    action: { type: String, required: true },
+    actionId: { type: Schema.Types.ObjectId, required: true },
+    actionType: { type: String, enum: ["question", "answer"], required: true },
+  },
+  { timestamps: true }
+);
+
+const Interaction = models?.interaction || model<IInteraction>("Interaction", interactionSchema);
+
+export default Interaction;
