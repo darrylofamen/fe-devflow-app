3203b72f68ba2a8ebee84e4fc6c52e4c7475ac05
Darryl <darryl.ofamen@gmail.com>
2025-11-17 18:40:47 +0800

    Create Answer model

---
+++ database/answer.model.ts
@@ -0,0 +1,21 @@
+import { Document, model, models, Schema, Types } from "mongoose";
+
+export interface IAnswer extends Document {
+  author: Types.ObjectId;
+  question: Types.ObjectId;
+  content: string;
+  upvotes: number;
+  downvotes: number;
+}
+
+const answerSchema = new Schema<IAnswer>({
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+  content: { type: String, required: true },
+  upvotes: { type: Number, default: 0 },
+  downvotes: { type: Number, default: 0 },
+});
+
+const Answer = models?.answer || model<IAnswer>("Answer", answerSchema);
+
+export default Answer;
