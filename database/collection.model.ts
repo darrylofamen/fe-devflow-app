21b50e91a39128324f24ddecefa620ca91d85e5b
Darryl <darryl.ofamen@gmail.com>
2025-11-17 19:20:09 +0800

    Create Collection model

---
+++ database/collection.model.ts
@@ -0,0 +1,18 @@
+import { Document, model, models, Schema, Types } from "mongoose";
+
+export interface ICollection extends Document {
+  author: Types.ObjectId;
+  question: Types.ObjectId;
+}
+
+const collectionSchema = new Schema<ICollection>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+  },
+  { timestamps: true }
+);
+
+const Collection = models?.collection || model<ICollection>("Collection", collectionSchema);
+
+export default Collection;
