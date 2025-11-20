2ea29a9ea6d6ad11a06722bb978d0def28a88e37
Darryl <darryl.ofamen@gmail.com>
2025-11-20 10:02:15 +0800

    Implement User By Email

---
+++ app/api/users/email/route.ts
@@ -0,0 +1,22 @@
+import handleError from "@/lib/handlers/error";
+import { APIErrorResponse } from "@/types/global";
+import { UserSchema } from "@/lib/validation";
+import User from "@/database/user.model";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { email } = body;
+
+  try {
+    const validatedData = UserSchema.partial().safeParse({ email });
+    if (!validatedData.success) throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+    const user = await User.findOne({ email });
+    if (!user) throw new NotFoundError("User");
+
+    return Response.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
