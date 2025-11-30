52baa4472c33d5941f3537e813833d7bcf41ee3f
Darryl <darryl.ofamen@gmail.com>
2025-11-30 11:14:24 +0800

    Implement getTags server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { getTags } from "@/lib/actions/tag.actions";
 
-const Tags = () => {
-  return (
-    <div>Tags</div>
-  );
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "nextjs",
+  });
+
+  const { tags } = data || {};
+
+  return <div>{JSON.stringify(tags, null, 2)}</div>;
 };
 export default Tags;
